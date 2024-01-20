37f5074d4ad1dc76758cc3d0062bad3be0f19367
Aliaksei Kirykovich <lexkirik@mail.ru>
2024-01-20 17:02:22 +0300

    displayed cell info

--- TaskPlanner/ListViewController/ReminderListViewController.swift
+++ TaskPlanner/ListViewController/ReminderListViewController.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
-    
+
     var dataSource: DataSource!
 
     override func viewDidLoad() {
@@ -19,13 +17,7 @@ class ReminderListViewController: UICollectionViewController {
         let listlayout = listlayout()
         collectionView.collectionViewLayout = listlayout
         
-        let cellRegistration = UICollectionView.CellRegistration {
-            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String ) in
-            let reminder = Reminder.sampleData[indexPath.item]
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = reminder.title
-            cell.contentConfiguration = contentConfiguration
-        }
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         
         dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
