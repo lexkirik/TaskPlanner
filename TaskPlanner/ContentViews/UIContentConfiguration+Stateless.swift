cba1e8f6e00d21893b5e219187e4fcebc224ebbe
Aliaksei Kirykovich <lexkirik@mail.ru>
2024-01-22 14:57:39 +0300

    used content views in editing mode

---
+++ TaskPlanner/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  TaskPlanner
+//
+//  Created by Test on 22.01.24.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
